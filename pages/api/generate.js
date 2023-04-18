1e396612fcc57cf9103325ed4eae98e3f7ccf412
Mokhalad <mjayalju12234@gmail.com>
2023-04-18 03:23:17 -0700

    added user prompt to send to api (no prompt prefix)

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,11 +8,11 @@ const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = "";
 const generateAction = async (req, res) => {
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+  const prompt = `${basePromptPrefix}${req.body.userInput}`;
 
   const baseCompletion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${basePromptPrefix}`,
+    prompt: `${prompt}`,
     temperature: 0.7,
     max_tokens: 250,
   });
